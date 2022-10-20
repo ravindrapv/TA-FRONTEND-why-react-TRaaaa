7c8fb1a4ecfe8d13468889021665ad185e0a9c7c
ravindrapv <ravindra.ravi4849@gmail.com>
2022-10-20 22:46:16 +0530

    color-codes

---
+++ block-BRaaay/color-code/src/components/Colors.js
@@ -0,0 +1,11 @@
+function Colors(){
+    return(
+        <>
+            <div className="container">
+                <h1>colors</h1>
+            </div>
+        </>
+    )
+}
+
+export default Colors;
